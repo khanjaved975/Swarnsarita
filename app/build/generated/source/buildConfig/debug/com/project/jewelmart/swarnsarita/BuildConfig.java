ad24ecd3e6878ce489d2724fd3b8c7cc4bb3bce1
khanjaved975 <khanjaved975@gmail.com>
2020-12-03 13:27:47 +0530

    Customization

--- app/build/generated/source/buildConfig/debug/com/project/jewelmart/swarnsarita/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/project/jewelmart/swarnsarita/BuildConfig.java
@@ -4,10 +4,9 @@
 package com.project.jewelmart.swarnsarita;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.project.jewelmart.swarnsarita";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 9;
-  public static final String VERSION_NAME = "1.8";
+  public static final int VERSION_CODE = 13;
+  public static final String VERSION_NAME = "2.2";
 }
